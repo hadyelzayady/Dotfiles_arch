86a7cd1c3dec08565b7b123a5195dc0106279158
HadyElzayady <hadyelzayady@yahoo.com>
2020-12-18 11:20:50 +0200

    install.sh script to configure fresh arch linux installation

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd
+sudo pacman -Sy git xorg xorg-xinit sxhkd npm
+git clone https://github.com/hadyelzayady/dwm.git
+(cd dwm && sudo make clean install)
+git clone https://github.com/hadyelzayady/Dotfiles_arch.git $HOME
+
+git clone https://github.com/hadyelzayady/dmenu.git
+(cd dmenu && sudo make clean install)
+git clone https://github.com/hadyelzayady/st.git
+(cd st && sudo make clean install)
+
+git clone https://github.com/hadyelzayady/config_folder.git $HOME/.config
+
+sudo pacman -S python python-pip xclip ctags bash-language-server numlockx feh flameshot acpilight dunst
+sudo pip install neovim
+sudo npm install neovim
+
+git clone https://github.com/hadyelzayady/scripts.git $HOME/.local/bin/scripts
